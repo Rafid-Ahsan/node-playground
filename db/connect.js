ccfeb09aa5e1eaf619d5dafc5639bbc7128aedfa
Rafid Ahsan <rafidahsan@Rafids-MacBook-Pro.local>
2021-11-21 02:36:57 +0600

    DB connection done with no warning

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 const connectionString = 'mongodb+srv://root:root@nodeplayground.08sme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-mongoose.connect(connectionString)
+mongoose
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log('Connect to Db...'))
     .catch((err) => console.log(err))
