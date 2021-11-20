098c33d79568ec920113fb3b76b0160afb5481bd
Rafid Ahsan <rafidahsan@Rafids-MacBook-Pro.local>
2021-11-21 02:58:22 +0600

    DB Connection Code format

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('./db/connect')
-
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 // middlewares
 app.use(express.json())
@@ -16,5 +16,13 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3600
 
-app.listen(port, console.log(`Server is listening on port ${port}...`))
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    }   catch (error) {
+        console.log(error)
+    }
+}
 
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://root:root@nodeplayground.08sme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const connectionString = ''
 
-mongoose
-    .connect(connectionString, {
+const connectDB = (url) => {
+    return mongoose.connect(url, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: true,
         useUnifiedTopology: true
     })
-    .then(() => console.log('Connect to Db...'))
-    .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
