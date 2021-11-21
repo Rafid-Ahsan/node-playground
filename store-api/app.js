0820f0a7f6e5a6bb29ee7e1c7c0453f1347ea4ea
Rafid Ahsan <rafidahsan@Rafids-MacBook-Pro.local>
2021-11-22 00:54:01 +0600

    Connect to DB

--- store-api/app.js
+++ store-api/app.js
@@ -1,1 +1,41 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async errors
+
+
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+
+// custom middlewares
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+// default middlewares
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>')
+})
+
+// products routes
+
+
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3600
+const start = async () => {
+    try {
+        // connectDB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is Listening on port: ${port}`))
+    }   catch (error) {
+        console.log(error)
+    }
+}
+
+start()
